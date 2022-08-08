0365e338cc551d1f1d9971c9c7fd2a10055d6e1d
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-08 16:15:22 -0400

    Exercise 1 and 2 done

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n');
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n');
+
+for (let i = 0; i <= 100; i++) {
+    if (i%2 == 0) {
+        console.log(i);
+    }
+
+}
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log('EXERCISE 2:\n==========\n');
+
+for (let i = 0; i <= 100; i++) {
+	if (i % 3 == 0) {
+		console.log('FIZZ');
+	} else if (i % 5 == 0) {
+		console.log('BUZZ');
+	} else if (i % 3 == 0 && i % 5 == 0) {
+		console.log('FIZZBUZZ');
+	} else {
+		console.log(i);
+	}
+}
