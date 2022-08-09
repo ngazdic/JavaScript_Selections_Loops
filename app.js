da6a44de67ff132969a65dbdbaa3ca811193cd64
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-08-09 10:58:17 -0400

    Done but needs to be fixed

--- app.js
+++ app.js
@@ -12,25 +12,37 @@ for (let i = 0; i <= 100; i++) {
 // Exercise 2 Section
 console.log('EXERCISE 2:\n==========\n');
 
-for (let i = 0; i <= 100; i++) {
-	if (i % 3 == 0) {
+for (let i = 0; i < 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0) {
+		console.log('FIZZBUZZ');
+	} else if (i % 3 == 0) {
 		console.log('FIZZ');
 	} else if (i % 5 == 0) {
 		console.log('BUZZ');
-	} else if (i % 3 == 0 && i % 5 == 0) {
-		console.log('FIZZBUZZ');
 	} else {
 		console.log(i);
 	}
 }
 
+//Exercise 3 Section
+let index1 = 1;
+while (index1 <= 100) {
+	if (index1 % 2 != 0) {
+		console.log(index1);
+	}
+	index++;
+}
+
 // Exercise 4 Section
 
 let value = Math.round(Math.random() * 500);
-for (let i = 0; i < 500; i++) {
+let n = Math.round(Math.random() * (500 - 100) + 100);
+let result = '';
+for (let i = 0; i < n; i++) {
 	if (i == value) {
 		console.log('Found Value');
 	} else {
 		console.log('Did not find value');
 	}
 }
+// Exercise 4 Section
